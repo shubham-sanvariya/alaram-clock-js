155b9a542aaa92b702c8c3926926749867e1f726
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-07-04 11:42:32 +0530

    adding form section for select and options are added by js function

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 
-
+const setHours = document.querySelector("#hours");
+const setMinutes = document.querySelector("#minutes");
+const setSeconds = document.querySelector("#seconds");
+const setAmPm = document.querySelector("#am-pm");
+const setAlarmButton = document.querySelector("#set_alarm");
 const time = document.querySelector("#time");
 
 window.addEventListener("DOMContentLoaded", () => {
 
+    dropDownMenu(1, 12, setHours);
+
+    dropDownMenu(0, 59, setMinutes);
+
+    dropDownMenu(0, 59, setSeconds);
+
     setInterval(fetch_time, 1000);
 });
+
+function dropDownMenu(start, end, element) {
+    for (let i = start; i <= end; i++) {
+        const dropDown = document.createElement("option");
+        val = i.toString().padStart(2, '0');
+        dropDown.value = val;
+        dropDown.innerHTML = val;
+        element.appendChild(dropDown);
+    }
+}
 
 function fetch_time() {
     const now = new Date();
